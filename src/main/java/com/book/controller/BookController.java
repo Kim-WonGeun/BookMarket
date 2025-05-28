7e0439dba095a20417bde459c7cf00437bf9a358
wg <wgkim9310@gmail.com>
2025-05-28 13:54:40 +0900

    - BookController Update

--- src/main/java/com/book/controller/BookController.java
+++ src/main/java/com/book/controller/BookController.java
@@ -1,22 +1,23 @@
 package com.book.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import com.book.domain.Book;
 import com.book.service.BookService;
 
 @Controller
+@RequestMapping("/books")
 public class BookController {
 
 	@Autowired
 	private BookService bookService;
 	
-	@RequestMapping(value="/books" , method=RequestMethod.GET)
+	@GetMapping
 	public String requestBookList(Model model) {
 		
 		List<Book> list = bookService.getAllBookList();
@@ -25,5 +26,19 @@ public class BookController {
 		return "books";
 		
 	}
+
+	@GetMapping("/all")
+	public ModelAndView requestAllBook() {
+		
+		ModelAndView modelAndView = new ModelAndView();
+		
+		List<Book> list = bookService.getAllBookList();
+		
+		modelAndView.addObject("bookList", list);
+		modelAndView.setViewName("books");
+		
+		return modelAndView;
+		
+	}	
 	
 }
