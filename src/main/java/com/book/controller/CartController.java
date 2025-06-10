27f7996b98ea6f897503187e4e18fde75190a609
wg <wgkim9310@gmail.com>
2025-06-10 20:10:44 +0900

    - Cart Item Delete

--- src/main/java/com/book/controller/CartController.java
+++ src/main/java/com/book/controller/CartController.java
@@ -85,4 +85,45 @@ public class CartController {
 		
 	}
 	
+//	@PutMapping("/remove/{bookId}")
+//	@ResponseStatus(value=HttpStatus.NO_CONTENT)	
+//	public void removeCartByItem(@PathVariable String bookId, HttpServletRequest request) {
+//		System.out.println("controller start ===============");
+//
+//		// 장바구니 ID인 세션ID 가져오기
+//		String sessionId = request.getSession(true).getId();
+//		
+//		Cart cart = cartService.read(sessionId);	// 장바구니에 등록된 모든 정보 얻어 오기
+//
+//		if(cart == null) {
+//			cart = cartService.create(new Cart(sessionId));
+//		}
+//
+//		// 경로 변수 bookId에 대한 정보 얻어 오기
+//		Book book = bookService.getBookById(bookId);
+//		
+//		if(book == null) {
+//			throw new IllegalArgumentException(new BookIdException(bookId));
+//		}
+//		
+//		// bookId에 대한 도서 정보를 장바구니에 등록하기
+//		cart.removeCartItem(new CartItem(book));
+//		cartService.update(sessionId, cart);	// 세션 ID에 대한 장바구니 갱신하기
+//		
+//	}
+
+    @PutMapping("/remove/{bookId}")  
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void removeCartByItem(@PathVariable String bookId, HttpServletRequest request) {
+        String sessionId = request.getSession(true).getId();  
+        Cart cart = cartService.read(sessionId);  
+        if(cart== null)
+            cart = cartService.create(new Cart(sessionId));
+            Book book = bookService.getBookById(bookId);  
+        if(book == null)
+            throw new IllegalArgumentException(new BookIdException(bookId));
+        cart.removeCartItem(new CartItem(book));  
+        cartService.update(sessionId, cart);  
+    }  
+	
 }
--- src/main/webapp/WEB-INF/views/cart.jsp
+++ src/main/webapp/WEB-INF/views/cart.jsp
@@ -1,12 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
     
 <!DOCTYPE html>
 <html>
 <head>
 <title>Cart</title>
 <link href="<c:url value="/resources/css/bootstrap.min.css"/>" rel="stylesheet">
+<script src="<c:url value="/resources/js/controllers.js"/>"></script>
 </head>
 
 <body>
@@ -38,14 +40,17 @@
 				<th>소계</th>
 				<th>비고</th>
 			</tr>
-			<c:forEach items="${cart.cartItems}" var="item">
-			<tr>
-				<td>${item.value.book.bookId}-${item.value.book.name}</td>
-				<td>${item.value.book.unitPrice}</td>
-				<td>${item.value.quantity}</td>
-				<td>${item.value.totalPrice}</td>
-			</tr>
-			</c:forEach>
+		  	<form:form name="removeForm" method="put">
+		            <c:forEach items="${cart.cartItems}" var="item">
+		            <tr>
+		                <td>${item.value.book.bookId}-${item.value.book.name}</td>
+		                <td>${item.value.book.unitPrice}</td>
+		                <td>${item.value.quantity}</td>
+		                <td>${item.value.totalPrice}</td>
+		                <td><a href="javascript:removeFromCart('../cart/remove/${item.value.book.bookId}')" class="badge badge-danger">삭제</a></td> 
+		            </tr>
+            </c:forEach>
+        	</form:form>
 			<tr>
 				<th></th>
 				<th></th>
